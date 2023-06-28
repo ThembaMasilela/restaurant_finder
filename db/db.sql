2b6ebaa983e253368f6e7c4d21bc4fbd85417102
ThembaMasilela <21062120+ThembaMasilela@users.noreply.github.com>
2023-06-28 16:18:47 +0200

    update package

--- db/db.sql
+++ db/db.sql
@@ -1,3 +1,11 @@
+CREATE TABLE restaurants
+(
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check (price_range >= 1 AND price_range <= 5)
+)
+
 CREATE TABLE reviews (
     id BIGSERIAL NOT NULL PRIMARY KEY,
     restaurant_id BIGINT NOT NULL REFERENCES restaurants(id),
@@ -8,6 +16,7 @@ CREATE TABLE reviews (
         and rating <= 5
     )
 );
+
 select *
 from restaurants
     left join(
